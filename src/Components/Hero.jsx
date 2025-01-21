ba5fed37ef0fbbebc9d0b4879ef41c48d422e0f4
mahmoudReactABS <mmotaz.absai@yahoo.com>
2025-01-21 15:11:09 +0200

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
                 <div className='mask w-100 container px-5'>
                     <div className='w-100 container' data-aos="fade-right" data-aos-delay="300">
                         <h1 className={`${direction} mb-3 fw-bolder`}>{t('public')} <br /> <span className='text-primary'>{t('cons')}</span></h1>
-                        <p className={`${direction} mb-3 fs-5`}>{t('par')}</p>
+                        <p className={`${direction} mb-3 fs-5 fw-bolder`}>{t('par')}</p>
                         <button className='btn btn-md' onClick={() => navigate('/contacts')} href='#!' role='button'>
                             {t('contactUs')}
                         </button>
