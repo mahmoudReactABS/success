9d4d1b686fec36d633f196653c81f03b64988bb9
mahmoudReactABS <mmotaz.absai@yahoo.com>
2025-01-21 13:58:20 +0200

    Add files via upload

--- src/Components/messageForm.jsx
+++ src/Components/messageForm.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { useTranslationContext } from '../translationContext';
 
 function MessageForm(props) {
-    const { t, i18n } = useTranslationContext();
+    const { t, i18n } = useTranslationContext(),
+    txt_dir=i18n.language == 'en' ? "text-start":"text-end";
 
     const [formData, setFormData] = useState({
         fullName: '',
@@ -34,25 +35,25 @@ function MessageForm(props) {
     return (
         <>
             {props.align != 'ups' ?
-                <form className='row text-start'>
+                <form className={`row ${txt_dir}`}>
                     <section className='col-xl-6 col-lg-6 col-md-12 col-sm-12'>
                         <div className='w-100 my-3'>
-                            <h6 className='fw-bold'>{t('fName')}</h6>
+                            <h6>{t('fName')}</h6>
                             <input required className='bg-transparent form-control w-100' type='text' name='fullName' value={formData.fullName} onChange={handleInputChange} />
                         </div>
                         <div className='w-100 my-3'>
-                            <h6 className='fw-bold'>{t('phoneNo')}</h6>
+                            <h6>{t('phoneNo')}</h6>
                             <input required className='bg-transparent form-control w-100' type='tel' name='phoneNumber' value={formData.phoneNumber} onChange={handleInputChange} />
                         </div>
                         <div className='w-100 my-3'>
-                            <h6 className='fw-bold'>{t('mail')}</h6>
+                            <h6>{t('mail')}</h6>
                             <input required className='bg-transparent form-control w-100' type='email' name='email' value={formData.email} onChange={handleInputChange} />
                         </div>
                     </section>
 
                     <section className='col-xl-6 col-lg-6 col-md-12 col-sm-12'>
                         <div className="my-3">
-                            <h6 className='fw-bold'>{t('message')}</h6>
+                            <h6>{t('message')}</h6>
                             <textarea required style={{ resize: 'none' }} rows={'8'} className='bg-transparent form-control w-100' name='message'
                                 value={formData.message} onChange={handleInputChange}></textarea>
                         </div>
@@ -61,25 +62,25 @@ function MessageForm(props) {
                     <button className='btn w-25 mx-auto py-3 fs-5' type='submit' disabled={isButtonDisabled}>
                         {t('sub')}
                     </button>
-                </form> : <form className='text-start'>
+                </form> : <form className={txt_dir}>
                     <section className='row'>
                         <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12 my-3'>
-                            <h6 className='fw-bold'>{t('fName')}</h6>
+                            <h6>{t('fName')}</h6>
                             <input required className='bg-transparent form-control w-100' type='text' name='fullName' value={formData.fullName} onChange={handleInputChange} />
                         </div>
                         <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12 my-3'>
-                            <h6 className='fw-bold'>{t('phoneNo')}</h6>
+                            <h6>{t('phoneNo')}</h6>
                             <input required className='bg-transparent form-control w-100' type='tel' name='phoneNumber' value={formData.phoneNumber} onChange={handleInputChange} />
                         </div>
                     </section>
 
                     <section className=''>
                         <div className='w-100 my-3'>
-                            <h6 className='fw-bold'>{t('mail')}</h6>
+                            <h6>{t('mail')}</h6>
                             <input required className='bg-transparent form-control w-100' type='email' name='email' value={formData.email} onChange={handleInputChange} />
                         </div>
                         <div className="my-3">
-                            <h6 className='fw-bold'>{t('message')}</h6>
+                            <h6>{t('message')}</h6>
                             <textarea required style={{ resize: 'none' }} rows={'8'} className='bg-transparent form-control w-100' name='message'
                                 value={formData.message} onChange={handleInputChange}></textarea>
                         </div>
